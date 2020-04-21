08e4dec3d5663d83b879e55febcc3ad5281b1272
Administrator <2358764726@qq.com>
2020-04-22 00:56:49 +0800

    “2020-04-21： properties文件、logback、多环境配置、 热部署配置、HTTP and  HTTPS”

---
+++ springBootDemo2023/src/main/java/com/example/demo/config/ServletAndWeb.java
@@ -0,0 +1,39 @@
+package com.example.demo.config;
+
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AutoConfigureAfter({WebMvcAutoConfiguration.class})
+public class ServletAndWeb {
+	
+	@Value("${spring.http}")
+	private int httpPort;
+	
+	@Bean
+	public Connector connector(){
+			Connector connector = new Connector();
+			connector.setScheme("http");
+			connector.setPort(80);
+			
+			
+			return connector;
+	}
+	
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory(){
+		TomcatServletWebServerFactory  tamcatFactory  = new TomcatServletWebServerFactory();
+		//添加额外的Tomcat连接器
+		tamcatFactory.addAdditionalTomcatConnectors(connector());
+		
+		return 	tamcatFactory;
+	}
+
+}
--- springBootDemo2023/src/main/java/com/example/demo/modules/test/controller/TestController.java
+++ springBootDemo2023/src/main/java/com/example/demo/modules/test/controller/TestController.java
@@ -1,13 +1,54 @@
 package com.example.demo.modules.test.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.example.demo.modules.test.vo.ConfigBean;
+
+@RestController
+@RequestMapping("/api/Controller")
 public class TestController {
+	//这是用全局配置文件 测试
+	//通过value这个注解的方式将值传递过来
+	@Value("${server.port}")
+	private int port;
+	@Value("${com.thron_bird.name}")
+	private String name;
+	@Value("${com.thron_bird.age}")
+	private int age;
+	@Value("${com.thron_bird.desc}")
+	private String desc;
+	@Value("${com.thron_bird.random}")
+	private String random;
 	
-	@RequestMapping("/Controller/appDoesc")
+	
+	//注入我们创建的非全局配置文件测试类
+	@Autowired
+	private ConfigBean configBean;
+	
+
+	@RequestMapping("/config")
+	@ResponseBody
+	public String configTest(){
+		StringBuffer sb = new StringBuffer();
+		sb.append(port).append("------")
+			.append(name).append("------")
+			.append(age).append("------")
+			.append(desc).append("------")
+			.append(random).append("</br>");
+			sb.append(configBean.getName()).append("------")
+				.append(configBean.getAge()).append("------")
+				.append(configBean.getDesc()).append("------")
+				.append(configBean.getRandom());
+		return sb.toString();
+	}
+	
+	@RequestMapping("/appDoesc")
+	//@ResponseBody的作用是讲该方法变成接口与controller一样
 	@ResponseBody
 	public String getDoesc(){
 		return "Holle word, this is spring boot demo.";
