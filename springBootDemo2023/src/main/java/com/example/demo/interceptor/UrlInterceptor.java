13de5c9710bfea4d5a2d117d8fc7deda6bfdefa3
Administrator <2358764726@qq.com>
2020-05-05 11:04:38 +0800

    "2020-05-04 :  aop 上传下载"

--- springBootDemo2023/src/main/java/com/example/demo/interceptor/UrlInterceptor.java
+++ springBootDemo2023/src/main/java/com/example/demo/interceptor/UrlInterceptor.java
@@ -3,6 +3,7 @@ package com.example.demo.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -30,11 +31,14 @@ public class UrlInterceptor implements HandlerInterceptor {
 		LOGGER.debug("post ---------------------------- ");
 		
 		
+		if(modelAndView == null || modelAndView.getViewName().startsWith("redirect")){
+			return;
+		}
 		
 		String uri = request.getServletPath();
 		String template = (String) modelAndView.getModelMap().get("template");
 		
-		if(template == null || template ==""){
+		if(StringUtils.isNoneBlank(template)){
 			if(uri.startsWith("/")){
 				uri = uri.substring(1);
 			}
--- springBootDemo2023/src/main/java/com/example/demo/modules/test/controller/TestController.java
+++ springBootDemo2023/src/main/java/com/example/demo/modules/test/controller/TestController.java
@@ -1,12 +1,24 @@
 package com.example.demo.modules.test.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Paths;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,21 +48,115 @@ public class TestController {
 	@Value("${com.thron_bird.random}")
 	private String random;
 	
-	
 	//注入我们创建的非全局配置文件测试类
-	@Autowired
-	private ConfigBean configBean;
-	
-	@RequestMapping("/log")
-	public String logTest(){
-		LOGGER.trace("this is TRACE");
-		LOGGER.debug("this is DEBUG");
-		LOGGER.info("this is INFO");
-		LOGGER.warn("this is WARN");
-		LOGGER.error("this is ERROR log.");
-		return "this is log test";
-	}
+		@Autowired
+		private ConfigBean configBean;
+		
+		@RequestMapping("/log")
+		public String logTest(){
+			LOGGER.trace("this is TRACE");
+			LOGGER.debug("this is DEBUG");
+			LOGGER.info("this is INFO");
+			LOGGER.warn("this is WARN");
+			LOGGER.error("this is ERROR log.");
+			return "this is log test";
+		}
 
+		@RequestMapping("/download")
+		public ResponseEntity<Resource> downLoadFile(@RequestParam  String fileName){
+			try {
+				
+				Resource resource = new UrlResource(Paths.get("D:\\load\\"+fileName).toUri());
+				
+				return ResponseEntity.ok()
+						.header(HttpHeaders.CONTENT_TYPE, "application/octet-stream")
+						.header(HttpHeaders.CONTENT_DISPOSITION
+									, String.format("attachment;filename=\"%s\"", resource.getFilename()))
+						.body(resource);
+			} catch (Exception e) {
+				e.printStackTrace();	
+			}	
+			return null;
+		}	
+		
+		
+		
+		
+		/**
+		 * 将文件以BufferedInputStream的方式读取到byte[]里面，然后用OutputStream.write输出文件
+		 */
+		@RequestMapping("/download1")
+		public void downloadFile1(HttpServletRequest request, 
+				HttpServletResponse response, @RequestParam String fileName) {
+			String filePath = "D:/upload" + File.separator + fileName;
+			File downloadFile = new File(filePath);
+			
+			if (downloadFile.exists()) {
+				response.setContentType("application/octet-stream");
+				response.setContentLength((int)downloadFile.length());
+				response.setHeader(HttpHeaders.CONTENT_DISPOSITION, 
+						String.format("attachment; filename=\"%s\"", fileName));
+				
+				byte[] buffer = new byte[1024];
+				FileInputStream fis = null;
+				BufferedInputStream bis = null;
+				try {
+					fis = new FileInputStream(downloadFile);
+					bis = new BufferedInputStream(fis);
+					OutputStream os = response.getOutputStream();
+					int i = bis.read(buffer);
+					while (i != -1) {
+						os.write(buffer, 0, i);
+						i = bis.read(buffer);
+					}
+				} catch (Exception e) {
+					LOGGER.debug(e.getMessage());
+					e.printStackTrace();
+				} finally {
+					try {
+						if (fis != null) {
+							fis.close();
+						}
+						if (bis != null) {
+							bis.close();
+						}
+					} catch (Exception e2) {
+						LOGGER.debug(e2.getMessage());
+						e2.printStackTrace();
+					}
+				}
+			}
+		}
+		
+		/**
+		 * 以包装类 IOUtils 输出文件
+		 */
+		@RequestMapping("/download2")
+		public void downloadFile2(HttpServletRequest request, 
+				HttpServletResponse response, @RequestParam String fileName) {
+			String filePath = "D:/upload" + File.separator + fileName;
+			File downloadFile = new File(filePath);
+			
+			try {
+				if (downloadFile.exists()) {
+					response.setContentType("application/octet-stream");
+					response.setContentLength((int)downloadFile.length());
+					response.setHeader(HttpHeaders.CONTENT_DISPOSITION, 
+							String.format("attachment; filename=\"%s\"", fileName));
+					
+					InputStream is = new FileInputStream(downloadFile);
+					IOUtils.copy(is, response.getOutputStream());
+					response.flushBuffer();
+				}
+			} catch (Exception e) {
+				LOGGER.debug(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+		
 	@RequestMapping("/config")
 	public String configTest(){
 		StringBuffer sb = new StringBuffer();
